bb59b551574e43a0f626b40449a3e73f82a2a8bc
IgBohdan <guestgeron@gmail.com>
2025-05-18 15:26:47 +0300

    app refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
-import Notification from "./components/Notification/Notification.JSX";
+import Notification from "./components/Notification/Notification";
 import Options from "./components/Options/Options";
 function App() {
   const [feedback, setFeedback] = useState(() => {
